ec4bc55c0b620c8b319f4ce98efa21092e04f605
Kavya I <24f1000013@ds.study.iitm.ac.in>
2026-06-17 04:49:44 +0530

    Implement CRUD operations using spring boot

--- crud/repository/UserR.java
+++ crud/repository/UserR.java
@@ -3,5 +3,6 @@ package com.example.crud.repository;
 import com.example.crud.entity.UserE;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface UserR extends JpaRepository<UserE, Integer> {
 }
