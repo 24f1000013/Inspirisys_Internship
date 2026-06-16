969aa3252f15bf1dc2aa4b7dcf8ffd995e43b9da
Kavya I <24f1000013@ds.study.iitm.ac.in>
2026-06-17 04:46:21 +0530

    Implement CRUD operations using spring boot

--- crud/entity/UserE.java
+++ crud/entity/UserE.java
@@ -1,36 +1,16 @@
-package com.example.crud.entity;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserE {
 
     @Id
     private int id;
     private String name;
-
-    public UserE() {
-    }
-
-    public UserE(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
